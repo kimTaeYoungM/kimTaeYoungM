f06490f763cdf0a872e50011bcdc82f8f530e7d9
kimTaeYoungM <154116060+kimTaeYoungM@users.noreply.github.com>
2023-12-22 23:16:38 +0900

    파이썬으로 배우는 누구나 코딩
    혼자 공부하는 파이썬 이후로 두번째 공부하는 전공 과목입니다!

---
+++ 예습/2-1 코딩.py
@@ -0,0 +1,11 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("원의 원주율 " + '3.141592')
+원의 원주율 3.141592
+>>> print("python " + 'programming ' + 'language')
+python programming language
+>>> print('파이썬 언어는' + " 강력하다")
+파이썬 언어는 강력하다
+>>> print('파이썬 ' + "언어! " + 3 * "방가 ")
+파이썬 언어! 방가 방가 방가 
+>>> 
---
+++ 예습/python_sample.py
@@ -0,0 +1,14 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World!")
+Hello World!
+>>> print("Python " + "program")
+Python program
+>>> print('방가' * 3)
+방가방가방가
+>>> print('콜록 ' * 4)
+콜록 콜록 콜록 콜록 
+>>> 
+================ RESTART: E:/파이썬 방학 공부/비주얼 스튜디오 파이썬/예습/2-1 코딩.py ===============
+원의 원주율 3.141592
+>>> 
